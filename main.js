63b6e306625929e63791049b0aa0a649ee2ce305
QuanNt21 <78184684+QuanNt21@users.noreply.github.com>
2023-12-21 06:36:34 +0700

    #6

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-const selectHome = document.querySelector(".home");
-const selectHearder = selectHome.querySelector(".hearder");
-const selectContainer = selectHome.querySelector(".container");
-const selectFooter = selectHome.querySelector(".footer");
-const selectBtnPower = selectFooter.querySelector(".footer-below .power");
+const $ = document.querySelector(".home");
+// header
+const selectHearder = $.querySelector(".hearder");
+const led2 = selectHearder.querySelectorAll(".hearder-center .led_2");
+
+// container
+const selectContainer = $.querySelector(".container");
 const screen = selectContainer.querySelector(".screen .show-information");
 const hide = selectContainer.querySelector(".screen .hide");
+const scale = selectContainer.querySelectorAll(".btn-scale input");
+const selectChannel = selectContainer.querySelectorAll(".nav-container_right .btns-blue-1 input");
+const selectedBe = selectContainer.querySelectorAll(".bullets .row-bullet .be");
+
+// footer
+const selectFooter = $.querySelector(".footer");
+const selectBtnPower = selectFooter.querySelector(".footer-below .power");
 const be = selectFooter.querySelectorAll(".range-btns .btns-white input");
+const shootingMode = selectFooter.querySelectorAll(".footer-below_left .shooting-mode input");
+const recognition = selectFooter.querySelectorAll(".footer-below_right .recognition input");
+
+console.log(selectedBe);
 
 const app = {
     handleEvents: function () {
@@ -13,14 +26,32 @@ const app = {
         // Xử lý khi click play
         // Handle when click play
         selectBtnPower.onclick = function () {
-            hide.classList.remove("hide"); //hide select box
-            screen.style.display = "block"; //show the playboard section
+            // console.log(scale[0]);
+            setTimeout(power, 2000);
+            function power() {
+                led2[0].classList.add("active");
+                scale[0].checked = true;
+                selectChannel[0].checked = true;
+                shootingMode[1].checked = true;
+                recognition[1].checked = true;
+                hide.classList.remove("hide"); //hide select box
+                screen.style.display = "block"; //show the playboard section
+            }
         };
+
+        for (var i = 0; i < scale.length; i++) {
+            scale[i].addEventListener("click", function (e) {
+                var currentScale = this.value;
+                console.log(currentScale);
+            });
+        }
 
         for (var i = 0; i < be.length; i++) {
             be[i].addEventListener("click", function (e) {
                 var currentBe = this.value;
-                console.log(currentBe);
+                var selectBe = selectedBe[currentBe - 1];
+                selectBe.style.background = "rgba(211, 211, 211, 0.3)";
+                console.log(selectBe);
             });
         }
     },
